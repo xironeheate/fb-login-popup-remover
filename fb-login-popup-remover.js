4299ecee415f79181481420f761fa5a08ec936d3
xironeheate <mkmichaelkuang@gmail.com>
2018-08-21 08:30:26 -0700

    Create fb-login-popup-remover.js

---
+++ fb-login-popup-remover.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         FB Login Popup Remover
+// @namespace    https://github.com/xironeheate/fb-login-popup-remover
+// @version      1.0
+// @description  remove facebook login popup
+// @author       XironeHeate
+// @match        https://*.facebook.com/*
+// @grant        none
+// @include      https://*.facebook.com/*
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    var popup = document.getElementsByClassName("_5hn6");
+    popup[0].remove();
+    document.getElementById("pagelet_growth_expanding_cta").remove();
+})();
